f014956144892610bf26b2b473d60d79c0cf5135
adeola2310 <adekoyejoadeola@gmail.com>
2021-04-15 19:53:33 +0100

    some changes

---
+++ Data Structures/Linkedlist/firstExample.js
@@ -0,0 +1,28 @@
+// assumoing we have an array of fruits
+
+const fruits = ['orange', 'apple', 'banana', 'grapes'];
+
+// how to have a fruit that is not an array but a linkedlist
+// linkedlist: orange --> apple --> banner
+
+
+// the end of a list points to null
+
+// linked list are better than array or hashtables
+// linkedlist are scattered all over memory while array are always located right to each other
+
+// iterating/traversing in a linkedlist is quite slower than iterating in an array. but the insert we can add in the middle is what
+// makes linkedlist better than using an array, you can also delete nodes in linkedlist easily than arrays. the nodes are in order as well compared to harshtables
+
+
+// prepend() O(1)
+// append() O(1)
+// lookup() O(n)
+// insert() O(n)
+// delete() O(n)
+
+// pointers are like reference in memory for example
+
+const student = {name: "adeola"};
+const refe = student
+console.log(refe);
